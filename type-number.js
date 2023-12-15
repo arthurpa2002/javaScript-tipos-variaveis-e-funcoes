114243ac1ba599082bf657c7ba5b45e95bfba730
Arthur Paschoalini de Azevedo <arthurp.wild@gmail.com>
2023-12-15 16:15:54 -0300

    Inicial

---
+++ type-number.js
@@ -0,0 +1,10 @@
+//tipos de numeros
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+ console.log(operacaoMatematica);
